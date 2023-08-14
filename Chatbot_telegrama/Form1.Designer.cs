f7dbdbf4b9df07a4b9501341231a1227e3d74c05
Thanh Tung <Tung2712itgts@gmail.com>
2023-08-14 14:32:08 +0700

    1

---
+++ Chatbot_telegrama/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿
+namespace Chatbot_telegrama
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.rtbResult = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(132, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(496, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ChatBot_Telegrama - Nhóm 14";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(278, 72);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(183, 43);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start Bot Telegram";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // rtbResult
+            // 
+            this.rtbResult.BackColor = System.Drawing.SystemColors.Window;
+            this.rtbResult.Location = new System.Drawing.Point(12, 121);
+            this.rtbResult.Name = "rtbResult";
+            this.rtbResult.ReadOnly = true;
+            this.rtbResult.Size = new System.Drawing.Size(717, 277);
+            this.rtbResult.TabIndex = 2;
+            this.rtbResult.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(741, 417);
+            this.Controls.Add(this.rtbResult);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Bot";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox rtbResult;
+    }
+}
+
---
+++ Chatbot_telegrama/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.InputFiles;
+
+namespace Chatbot_telegrama
+{
+    public partial class Form1 : Form
+    {
+        ITelegramBotClient botClient;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            botClient = new TelegramBotClient("2116338351:AAGoMTbdDQG2ANvjBOnhKiDHtZ5IENQS3bs");
+            botClient.OnMessage += BotClient_OnMessage;
+            var me = botClient.GetMeAsync().Result;
+            rtbResult.Text += $"Hello, Boss ! I am user {me.Id}\nMy name is {me.FirstName}.";
+            botClient.StartReceiving();
+            button1.Enabled = false;
+        }
+
+        private async void BotClient_OnMessage(object sender, MessageEventArgs e)
+        {
+            var id = e.Message.From.Id;
+            var test = e.Message.Text.ToString();
+            label1.BeginInvoke(new Action(() =>
+            {
+                rtbResult.Text += $"\nReceived a text message in chat {e.Message.Chat.Id}.";
+            }));
+
+            label1.BeginInvoke(new Action(() =>
+            {
+                rtbResult.Text += $"\nYou said Open: {test} ";
+            }));
+
+            switch (test)
+            {
+                case "Hello":
+                    await botClient.SendTextMessageAsync(id,
+                         "\n<b>Bot-Group14</b>\n" +
+                         "\nHello my friend" + "\nWhat can I do for you?",      //tôi có thể giúp gì cho bạn?
+                         Telegram.Bot.Types.Enums.ParseMode.Html, replyMarkup: null);
+                    break;
+                case "Hi":
+                    await botClient.SendTextMessageAsync(id,
+                         "\n<b>Bot-Group14</b>\n" +
+                         "\nHello my friend" + "\nWhat can I do for you?",      //tôi có thể giúp gì cho bạn?
+                         Telegram.Bot.Types.Enums.ParseMode.Html, replyMarkup: null);
+                    break;
+
+                case "How are you?":
+                    await botClient.SendTextMessageAsync(id,
+                         "\n<b>Bot-Group14</b>\n" +
+                         "\nIm good" +
+                         "\nAnd you?",
+                         Telegram.Bot.Types.Enums.ParseMode.Html, replyMarkup: null);
+                    break;
+
+                case "I'm fine":
+                    await botClient.SendTextMessageAsync(id,
+                         "\n<b>Bot-Group14</b>\n" +
+                         "\nGreat!",
+                         Telegram.Bot.Types.Enums.ParseMode.Html, replyMarkup: null);
+                    break;
+                case "I'm Ok":
+                    await botClient.SendTextMessageAsync(id,
+                         "\n<b>Bot-Group14</b>\n" +
+                         "\nGreat!",
+                         Telegram.Bot.Types.Enums.ParseMode.Html, replyMarkup: null);
+                    break;
+                case "I'm tired":
+                    await botClient.SendTextMessageAsync(id,
+                         "\n<b>Bot-Group14</b>\n" +
+                         "\nAre you unwell?!",
+                         Telegram.Bot.Types.Enums.ParseMode.Html, replyMarkup: null);
+                    break;
+
+
+                case "/notepad":
+
+                    await botClient.SendTextMessageAsync(id,
+                        "\n<b>Bot-Group14</b>\n" +
+                        "\nOk! You need me to open Notepad.",
+                       Telegram.Bot.Types.Enums.ParseMode.Html, replyMarkup: null);
+
+                    Process.Start("notepad");
+
+                    break;
+
+                case "/calc":
+
+                    await botClient.SendTextMessageAsync(id,
+                        "\n<b>Bot-Group14</b>\n" +
+                        "\nOk! You need me to open Calculator",
+                       Telegram.Bot.Types.Enums.ParseMode.Html, replyMarkup: null);
+
+                    Process.Start("calc");
+
+                    break;
+
+                case "/what_time_is_it":
+
+                    label1.BeginInvoke(new Action(() =>
+                    {
+                        rtbResult.Text += $"\n Vietnam ‎(UTC+7)‎: {DateTime.Now.ToString()} ";
+                    }));
+                    await botClient.SendTextMessageAsync(id,
+                        "\n<b>Bot-Group14</b>\n" +
+                         DateTime.Now.ToString(),
+                       Telegram.Bot.Types.Enums.ParseMode.Html, replyMarkup: null);
+
+                    break;
+
+                case "/capturescreen":
+
+                    await botClient.SendTextMessageAsync(id,
+                        "\n<b>Bot-Group14</b>\n" +
+                        "\nOk! I will take a screenshot",
+                       Telegram.Bot.Types.Enums.ParseMode.Html, replyMarkup: null);
+
+                    CaptureMyScreen();
+                    using (FileStream fs = System.IO.File.OpenRead("Capture.jpg"))
+                    {
+                        InputOnlineFile inputOnlineFile = new InputOnlineFile(fs, "Capture.jpg");
+                        var file = await botClient.SendDocumentAsync(e.Message.Chat, inputOnlineFile);
+                    }
+
+                    break;
+
+
+                default:
+                    await botClient.SendTextMessageAsync(id,
+                        "\n<b>Bot-Group14</b>\n" +
+                        "\nFunctions:" +
+                        "\nCalculator: /calc " +
+                        "\nNotepad: /notepad " +
+                        "\nDate & Time: /what_time_is_it " +
+                        "\nScreen capture:/capturescreen ",
+                       Telegram.Bot.Types.Enums.ParseMode.Html, replyMarkup: null);
+                    break;
+            }   
+           
+        }  
+        
+                private void CaptureMyScreen()
+        {
+            Rectangle bounds = Screen.PrimaryScreen.Bounds;
+            using (Bitmap bitmap = new Bitmap(bounds.Width, bounds.Height))
+            using (Graphics g = Graphics.FromImage(bitmap))
+            {
+                g.CopyFromScreen(new Point(bounds.Left, bounds.Top), Point.Empty, bounds.Size);
+                bitmap.Save("Capture.jpg", ImageFormat.Jpeg);
+            }
+        } 
+    }
+
+
+}
